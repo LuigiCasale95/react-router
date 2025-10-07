30c5df3099344a2f226d087cdb6e17986e1ee190
LuigiCasale95 <luigicasale95@gmail.com>
2025-10-07 17:04:28 +0200

    Collegamento axios per id in pagina prodotto

--- src/pages/PageProducts.jsx
+++ src/pages/PageProducts.jsx
@@ -1,14 +1,39 @@
+/* import axios */
+import axios from "axios";
+
+/* Import useState useEffect */
+import { useState, useEffect} from "react";
+
 /* Importi useParams */
 import { useParams } from "react-router-dom"
 
+
 const  PageProduct = () => {
+
+    const [product, setProduct] = useState({}) /* Per dare valore a product */
     
-    const { id } = useParams();
+    const { id } = useParams(); /* Per far ritornare l'informazione ad ID */
+
+
+    useEffect(() => {
+        axios.get(`https://fakestoreapi.com/products/${id}`)
+        .then((res) => setProduct(res.data))
+        .catch(error => console.log(error)
+        )
+    } 
+
+    )
 
     return (
         
         <div className="containerMy">
-            Pagina Del prodotto N {id} 
+        <div className="infoProduct p-2">
+
+           <h2> Pagina Del prodotto N: {id} </h2>
+           <img src={product.image} alt={product.name} />
+           <p>{product.description}</p>
+
+        </div>
         </div>
 
     )
